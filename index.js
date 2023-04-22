9a4571819678873d5fabcfa7a9229c284202fa84
Ayomide Taiwo <tosin0330@gmail.com>
2023-04-21 23:57:34 -0500

    myfiles.md

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(" ");
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(" ");
+  });
 }
+
+
